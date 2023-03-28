ce3d9c7495e4a605405de53ecf1cf884ae19b815
bendymend <Ryzhandanil@gmail.com>
2023-03-28 11:23:01 -0400

    Create portfolio project

---
+++ tailwind.config.js
@@ -0,0 +1,19 @@
+module.exports = {
+  content: ["./src/**/*.{js,jsx,ts,tsx}", "./index.html"],
+  theme: {
+    extend: {},
+    colors: {
+      transparent: 'transparent',
+      current: 'currentColor',
+  
+      'day': '#F8b195',
+      'midnight': '#355c6d',
+      'metal': '#565584',
+      'tahiti': '#3ab7bf',
+      'silver': '#ecebff',
+      'bubble-gum': '#ff77e9',
+      'bermuda': '#78dcca',
+    },
+  },
+  plugins: [],
+}
